1e2ef6f4f77484f18829a8ad85791f3cdce2ee99
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-01-04 14:20:27 +0100

    Update workflow script and remove old python scripts
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- sscs/in-toto/workflow.sh
+++ sscs/in-toto/workflow.sh
@@ -3,71 +3,29 @@
 # creating in-toto steps and layout.
 #
 # Output will be produced in the artifacts directory which includes the link
-# files, the layout, and the public key (in "json" format).
+# files, the layout, and the public key.
 GITHUB_ORG="${1:-trustification}"
 GITHUB_PROJECT="${2:-source-distributed}"
 GITHUB_TOKEN=$3
 PRIVATE_KEY=cosign.key
 PUBLIC_KEY=${PRIVATE_KEY}.pub.json
 
-commit_id=$(git branch --show-current)
-artifacts_dir="artifacts_work"
-artifacts_tar="${commit_id}.tar"
-artifacts_bundle="${commit_id}.bundle"
-verify_dir="verify"
+BRANCH=$(git branch --show-current)
+VERIFY_DIR="verify"
 
 echo "workflow.sh arguments: $GITHUB_ORG $GITHUB_PROJECT $GITHUB_TOKEN"
 
-## First generate the keypair to be used when signing
-echo "Generate keypair"
+pushd ../../ > /dev/null
+
+## Generate in-toto layout and link files
 if [ -z $GITHUB_TOKEN ]; then
-	echo "without token...."
-	cargo r --manifest-path=../../Cargo.toml --bin keygen
+	echo "Generating without token...."
+	cargo r --bin cargo-in-toto-gen -- -o $GITHUB_ORG -r $GITHUB_PROJECT
 else
-	echo "with token...."
-	cargo r --manifest-path=../../Cargo.toml --bin keygen $GITHUB_TOKEN
+	echo "Generating with token...."
+	cargo r --bin cargo-in-toto-gen -- -o $GITHUB_ORG -r $GITHUB_PROJECT --provider-token=$GITHUB_TOKEN
 fi
 
-# Change the pem tag to BEGIN ENCRYPTED COSIGN PRIVATE KEY, releated to
-# https://github.com/sigstore/sigstore-rs/pull/165
-sed -i 's/SIGSTORE/COSIGN/' cosign.key.enc
+cargo r --bin cargo-verify -- -d $GITHUB_PROJECT -a sscs/in-toto/artifacts/$BRANCH -p $PWD
 
-# Create keys in securesystemslib json key format. This will generate two
-# files: <keyname>.key.json and <keyname>.key.pub.json
-echo "Import and convert cosign keys to securesystemslib json format"
-./in-toto-key-import.py $PRIVATE_KEY
-
-mkdir -p $artifacts_dir
-echo "Create layout"
-./create-layout.py $GITHUB_ORG $GITHUB_PROJECT ${PRIVATE_KEY}.json $PUBLIC_KEY
-
-echo "Create steps"
-./create-steps.sh $GITHUB_ORG $GITHUB_PROJECT ${PRIVATE_KEY}.json $PUBLIC_KEY
-
-echo "Tar the artifacts"
-pushd $artifacts_dir > /dev/null
-tar -cvf $artifacts_tar *
-mv $artifacts_tar ../
 popd > /dev/null
-
-echo "Sign the $artifacts_tar with cosign"
-env COSIGN_PASSWORD="_" COSIGN_EXPERIMENTAL=1 cosign sign-blob -d --bundle $artifacts_bundle --key cosign.key.enc $artifacts_tar
-
-echo "Verify $artifacts_tar with cosign"
-env COSIGN_EXPERIMENTAL=1 cosign verify-blob --bundle=$artifacts_bundle $artifacts_tar
-
-mkdir -p ${verify_dir}
-cp $artifacts_tar $verify_dir
-echo "Verify the contents of $artifacts_tar"
-pushd $verify_dir > /dev/null
-tar xf $artifacts_tar
-rm $artifacts_tar
-in-toto-verify -v -t ecdsa --layout $GITHUB_PROJECT-layout.json --layout-keys=$PUBLIC_KEY
-popd > /dev/null
-
-rm -rf $verify_dir
-rm -rf $artifacts_dir
-rm cosign.*
-
-mkdir -p artifacts
-mv $artifacts_tar $artifacts_bundle artifacts
