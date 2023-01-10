4a429cdcbfcfd83b07670ce313f29fad3f4797df
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-01-10 08:48:37 +0100

    Remove support for securesystemslib json format
    This commit removes the support for securesystemslib json format which
    was used in previous revisions.
    
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- src/crypto.rs
+++ src/crypto.rs
@@ -14,16 +14,9 @@ use x509_parser::pem::parse_x509_pem;
 /// Parses the passed in string which is expected to be a private ec key in
 /// pem format, or in securesystemslib json format.
 pub fn priv_key_from_pem(s: &str) -> in_toto::Result<PrivateKey> {
-    let json_format: serde_json::Result<serde_json::Value> = serde_json::from_str(s);
-    if let Ok(_) = json_format {
-        let priv_key = PrivateKey::from_securesystemslib_ecdsa(s).unwrap();
-        return Ok(priv_key);
-    } else {
-        let (_, der) = parse_x509_pem(s.as_bytes()).unwrap();
-        let priv_key =
-            PrivateKey::from_pkcs8(&der.contents, SignatureScheme::EcdsaP256Sha256).unwrap();
-        return Ok(priv_key);
-    }
+    let (_, der) = parse_x509_pem(s.as_bytes()).unwrap();
+    let priv_key = PrivateKey::from_pkcs8(&der.contents, SignatureScheme::EcdsaP256Sha256).unwrap();
+    Ok(priv_key)
 }
 
 pub fn private_key_from_file(path: &PathBuf) -> PrivateKey {
