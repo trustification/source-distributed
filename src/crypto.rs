85f048722993c829d7cd81a316a19f4dac5a6a92
Daniel Bevenius <daniel.bevenius@gmail.com>
2023-02-02 15:06:11 +0100

    Add extract_subject function and use as challenge
    Signed-off-by: Daniel Bevenius <daniel.bevenius@gmail.com>

--- src/crypto.rs
+++ src/crypto.rs
@@ -27,53 +27,46 @@ pub fn private_key_from_file(path: &PathBuf) -> PrivateKey {
     priv_key_from_pem(&private_key_pem).unwrap()
 }
 
+fn extract_subject(token: &str, jwks_path: &str) -> String {
+    let header = decode_header(&token).unwrap();
+    let jwt_base64 = fs::read_to_string(jwks_path).unwrap();
+    let jwks: jwk::JwkSet = serde_json::from_str(&jwt_base64).unwrap();
+    let keyid = jwks.find(&header.kid.as_ref().unwrap());
+    if keyid.is_none() {
+        eprintln!(
+            "Could not find a matching key for keyid {:?}",
+            &header.kid.as_ref().unwrap()
+        );
+    }
+    let keyid = keyid.unwrap();
+    let subject = match &keyid.algorithm {
+        AlgorithmParameters::RSA(rsa) => {
+            let decoding_key = DecodingKey::from_rsa_components(&rsa.n, &rsa.e).unwrap();
+            let mut validation = Validation::new(keyid.common.algorithm.unwrap());
+            validation.validate_exp = false;
+            validation.validate_nbf = false;
+            let decoded_token =
+                decode::<HashMap<String, serde_json::Value>>(&token, &decoding_key, &validation)
+                    .unwrap();
+            decoded_token.claims.get("sub").unwrap().clone()
+        }
+        _ => unreachable!("this should be a RSA"),
+    };
+    subject.to_string()
+}
+
 pub async fn generate_keypair(token: Option<String>) -> Result<SigStoreKeyPair> {
     let token_provider = match token {
         Some(token) => {
-            let header = decode_header(&token).unwrap();
-            println!("{:?}", &header);
-            println!("typ: {:?}", &header.typ);
-            println!("alg: {:?}", &header.alg);
-            println!("kid: {:?}", &header.kid.as_ref().unwrap());
-            println!("x5t: {:?}", &header.x5t.unwrap());
-
-            let jwt_base64 = fs::read_to_string("jwks").unwrap();
-            let jwks: jwk::JwkSet = serde_json::from_str(&jwt_base64).unwrap();
-            if let Some(j) = jwks.find(&header.kid.unwrap()) {
-                match &j.algorithm {
-                    AlgorithmParameters::RSA(rsa) => {
-                        let decoding_key =
-                            DecodingKey::from_rsa_components(&rsa.n, &rsa.e).unwrap();
-                        let mut validation = Validation::new(j.common.algorithm.unwrap());
-                        validation.validate_exp = false;
-                        validation.validate_nbf = false;
-                        let decoded_token = decode::<HashMap<String, serde_json::Value>>(
-                            &token,
-                            &decoding_key,
-                            &validation,
-                        )
-                        .unwrap();
-                        println!("claims: {:?}", decoded_token.claims);
-                        println!("sub: {:?}", decoded_token.claims.get("sub").unwrap());
-                        println!("aud: {:?}", decoded_token.claims.get("aud").unwrap());
-                        println!("iss: {:?}", decoded_token.claims.get("iss").unwrap());
-                        println!(
-                            "job_workflow_ref: {:?}",
-                            decoded_token.claims.get("job_workflow_ref").unwrap()
-                        );
-                    }
-                    _ => unreachable!("this should be a RSA"),
-                }
-            } else {
-                eprintln!("No matching JWK found for the given kid");
-            }
-
+            // Just writing this the token to disk which can be useful for
+            // debugging a CI workflow
             fs::write("token", &token).unwrap();
+
+            // See ./notes.md##coreidtoken for details about the usage of the
+            // subject and JSON Web Key Sets (JWKS))
+            let subject = extract_subject(&token, "jwks");
             let id_token: CoreIdToken = CoreIdToken::from_str(&token).unwrap();
-            TokenProvider::Static((
-                id_token,
-                "repo:trustification/source-distributed:ref:refs/heads/main".to_string(),
-            ))
+            TokenProvider::Static((id_token, subject))
         }
         _ => TokenProvider::Oauth(OauthTokenProvider::default()),
     };
